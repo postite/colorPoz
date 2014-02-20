544e67d8b4c598d934889770e09fb97da52d2809
postite <blog@postite.com>
2014-02-20 23:29:49 +0100

    shortcut

--- CopyPlug.hx
+++ CopyPlug.hx
@@ -6,8 +6,9 @@
 
 	var t = sys.io.File.getContent( "colorPoz/out/colorPoz.jstalk" );
 		var f= new StringBuf();
-		f.add("// TestAPI (ctrl alt command c) \n");
+		f.add("// ColorPoz (ctrl alt command c) \n");
 		f.add(t);
+		sys.io.File.saveContent("colorPoz/out/colorPoz.jstalk" ,f.toString());
 		sys.io.File.saveContent("/Users/ut/Library/Application Support/sketch/Plugins/colorPoz/colorPoz.jstalk" ,f.toString());
 		//sys.io.File.copy("./out/testApi.jstalk", "/Users/ut/Library/Application Support/sketch/Plugins/TestApi/testApi.jstalk" );
 		
